fb268b21c14d8de31a617c46ca55eec78e912dfb
Kimd0ng <kdongh0406@gmail.com>
2024-12-12 05:25:41 +0900

    :sparkles: feat: header logo navigate 추가

--- src/components/header/LogoSection.jsx
+++ src/components/header/LogoSection.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logoImage from '../../assets/images/BottariLogo.svg';
 import * as S from './LogoSection.styles';
+import { useNavigate } from 'react-router-dom';
 
 function LogoSection() {
+  const navigate = useNavigate();
+  const handleLogo = () => {
+    navigate('/');
+  };
+
   return (
-    <S.Container>
+    <S.Container onClick={handleLogo}>
       <S.LogoImg src={logoImage} alt="Bottari Logo" />
     </S.Container>
   );
--- src/components/header/LogoSection.styles.jsx
+++ src/components/header/LogoSection.styles.jsx
@@ -3,6 +3,7 @@ import { styled } from 'styled-components';
 export const Container = styled.div`
   display: flex;
   height: 100%;
+  width: 100%;
 `;
 
 export const LogoImg = styled.img`
